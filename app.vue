3b0fe1e4d3282c1af1b7f5fc624cfc66c8d3355f
Oleh Didyk <oleggsa@gmail.com>
2022-12-27 12:39:08 +0200

    fonts connected

--- app.vue
+++ app.vue
@@ -8,10 +8,13 @@
 </template>
 
 <style lang="scss">
+@import "./assets/style/style.css";
+
 body {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+  font-family: Onest, sans-serif;
 }
 
 .container {
